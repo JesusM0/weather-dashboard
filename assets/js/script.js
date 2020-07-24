2f00d5219eb18070a17d50d5d7ab4e58b031f12f
Kos-Mos <ChuyM203@gmail.com>
2020-07-23 22:50:20 -0400

    added what i tried to work on

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const citySearchEl = document.querySelector(`#city-search`);
+const cityInputEl = document.querySelector(`#city-input`);
+const forecastContainerEl = document.querySelector(`forecast-container`);
+const todayHeaderEl = document.querySelector(`#todays-header`);
+const todayTempEl = document.querySelector("#today-temp");
+const todayHumidityEl = document.querySelector("#today-humidity");
+const todayWindSpeedEl = document.querySelector("#today-wind");
+
+
+function getApi() {
+    $.get
+}
